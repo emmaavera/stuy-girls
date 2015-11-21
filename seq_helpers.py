402ecf7eb9cc17eebb8fc956c7ba2ecb034def1b
emmaavera <emma.avera.stuy@gmail.com>
2015-11-21 16:07:27 -0500

    oof

--- seq_helpers.py
+++ seq_helpers.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 def random_seq( length ):
 	print "random_seq\n    returns a random sequence of length l"
 	bases = ['A','T','C','G']
@@ -8,13 +10,13 @@ def random_seq( length ):
 
 #takes two bases and returns true if complementary; does not account for U
 def comp( base1, base2 ):
-	if ( base1 == "A" && base2 == "T" )
+	if ( base1 == "A" and base2 == "T" )
 		return True
-	elif ( base1 == "T" && base2 == "A" )
+	elif ( base1 == "T" and base2 == "A" )
 		return True
-	elif ( base1 == "G" && base2 == "C" )
+	elif ( base1 == "G" and base2 == "C" )
 		return True
-	elif ( base1 == "C" && base2 == "G" )
+	elif ( base1 == "C" and base2 == "G" )
 		return True
 	else
 		return False
